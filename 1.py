a9972159a90338d03bf13147344d76a22d0fe8b5
cazyinfo <cazyinfo@gmail.com>
2025-03-18 18:05:59 +0900

    44

--- 1.py
+++ 1.py
@@ -10,7 +10,14 @@ df = pd.read_csv('1.csv', encoding='utf-8')
 # 取引種別が 'bid' の場合の行をフィルタリング
 bid_df = df[df['取引種別'] == 'bid']
 
-# 価格と数量の総和を計算
-total_value = (bid_df['価格'] * bid_df['数量']).sum()
+ask_df = df[df['取引種別'] == 'ask']
 
-print("価格 * 数量 の総和:", total_value)
+# 価格と数量の総和を計算
+bid_t = (bid_df['価格'] * bid_df['数量']).sum()
+ask_t = (ask_df['価格'] * ask_df['数量']).sum()
+
+print("bid 価格 * 数量 の総和:", bid_t)
+print("ask 価格 * 数量 の総和:", ask_t)
+su = 0; currrent_price=0
+now_su = su * currrent_price
+print("利益:", ask_t - bid_t + now_su)  # now_su: 現在保持しているコイン額 
